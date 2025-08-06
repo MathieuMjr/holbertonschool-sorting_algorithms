b729f3ac7bcb734015d805af1057d8efaeab7ee9
Mathieu <mathieu.mjr@gmail.com>
2025-08-06 20:53:17 +0200

    2nd version - shorter

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+/**
+ * swap - swap a node with another
+ * @node: a node to swap
+ * @next: with the next one
+ * @list: pointer to the head of the list
+ *
+ * Return:
+ */
 void swap(listint_t *node, listint_t *next, listint_t **list)
 {
 	listint_t *temp_prev;
@@ -16,52 +24,6 @@ void swap(listint_t *node, listint_t *next, listint_t **list)
 	next->next = node;
 	next->prev = temp_prev;
 }
-
-/**
- * swap_f - swap a node forward
- * @node: pointer to a node
- * @next: pointer to the next node
- *
- * Return:
- */
-/*void swap_f(listint_t *node, listint_t *next)
-{
-	listint_t *temp_prev;
-
-	temp_prev = node->prev;
-	if (node->prev != NULL)
-		node->prev->next = next;
-	if (next->next != NULL)
-		next->next->prev = node;
-	node->next = next->next;
-	node->prev = next;
-	next->next = node;
-	next->prev = temp_prev;
-}*/
-
-/**
- * swap_b - swap a node backward
- * @prev: prev node of the current node
- * @list: pointer to the head of a list
- *
- * Return:
- */
-/*void swap_b(listint_t *node, listint_t *prev, listint_t **list)
-{
-	listint_t *temp_next;
-
-	if (prev->prev != NULL)
-		prev->prev->next = node;
-	else
-		*list = node;
-	if (node->next != NULL)
-		node->next->prev = prev;
-	temp_next = node->next;
-	node->prev = prev->prev;
-	node->next = prev;
-	prev->prev = node;
-	prev->next = temp_next;
-}*/
 
 /**
  * insertion_sort_list - sort data in a
@@ -82,7 +44,6 @@ void insertion_sort_list(listint_t **list)
 		next = head_f->next;
 		if (head_f->n > next->n)
 		{
-			/*swap_f(head_f, next);*/
 			swap(head_f, next, list);
 			print_list(*list);
 			head_b = next;
@@ -91,7 +52,6 @@ void insertion_sort_list(listint_t **list)
 				prev = head_b->prev;
 				if (prev->n > head_b->n)
 				{
-					/*swap_b(head_b, prev, list);*/
 					swap(prev, head_b, list);
 					print_list(*list);
 				}
