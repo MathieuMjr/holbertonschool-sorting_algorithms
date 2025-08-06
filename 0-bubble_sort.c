856ded2408018b4490e167361875e9cc44f1a0fa
Mathieu <mathieu.mjr@gmail.com>
2025-08-06 15:56:38 +0200

    function sorting an array using bubble algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort data inside an array
+ * @array: adress of the array
+ * @size: number of element
+ *
+ * Return: height of the tree or -1 if
+ * tree is NULL
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int i;
+	int swap_counter = 1;
+	int temp;
+
+	while (swap_counter != 0)
+	{
+		swap_counter = 0;
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swap_counter++;
+				print_array(array, size);
+			}
+		}
+	}
+}
