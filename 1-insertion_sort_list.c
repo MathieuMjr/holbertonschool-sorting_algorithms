2c1514d5e295e1b66240a3669bef99030e99d5fe
Mathieu <mathieu.mjr@gmail.com>
2025-08-06 17:01:03 +0200

    work in progress

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort data in a 
+ * doubly linked list
+ * @list: pointer to the head of the list
+ *
+ * Return:
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *reading_head = *list;
+	listint_t *next = reading_head->next;
+
+	while (reading_head != NULL)
+	{
+		if (reading_head->n > )
+		reading_head = reading_head->next;
+	}
+}
